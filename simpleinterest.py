7c9abc7cbf244114772531056b3e666cac9ff489
mr-NK-18 <153973191+mr-NK-18@users.noreply.github.com>
2026-07-18 17:10:26 +0530

    Update simpleinterest.py

--- simpleinterest.py
+++ simpleinterest.py
@@ -1,1 +1,7 @@
+p = int(input("enter the principal amt: "))
+r = int(input("enter the rate of interest: "))
+t = int(input("enter the time in year: "))
 
+si = (p * r * t)/100
+
+print("the simple interest is: ",si)
