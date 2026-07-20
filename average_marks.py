2522f497dbe1478caa6fd6beb7764cc3f7ad2940
mr-NK-18 <153973191+mr-NK-18@users.noreply.github.com>
2026-07-20 16:11:53 +0530

    Add files via upload
    finding average marks using python

---
+++ average_marks.py
@@ -0,0 +1,8 @@
+tam=int(input("enter tamil marks"))
+eng=int(input("enter eng marks"))
+mat=int(input("enter math marks"))
+sci=int(input("enter science marks"))
+soc=int(input("enter evs marks"))
+
+avg=(tam+eng+mat+sci+soc)/5
+print("the average marks are => ",avg)
